5109652830119005f8b11a1d337dd7ca226d4a02
Johan Nilsson <github@markupartist.com>
2011-03-18 22:28:26 +0100

    Added another place to the route.

--- src/se/tidensavtryck/model/Route.java
+++ src/se/tidensavtryck/model/Route.java
@@ -61,12 +61,19 @@ public class Route {
         loc1.setLatitude(62.382413);
         loc1.setLongitude(17.337112);
         Place p1 = new Place("Hotel Knaust", loc1, 50, records);
+        
+        Location loc2 = new Location("se.tidensavtryck");
+        loc2.setLatitude(62.3908789765665);
+        loc2.setLongitude(17.3038633831725);
+        Place p2 = new Place("Bla bla bla plats", loc2, 50, records);
+
         LinkedList<Place> places = new LinkedList<Place>();
         places.add(p1);
+        places.add(p2);
         
         // route
         User creator = new User();
-        Route route = new Route("Knaust-rutten", "En trevlig tur runt kvarteret Knaust med historiska platser och fšremŒl.", creator, places);
+        Route route = new Route("Knaust-rutten", "En trevlig tur runt kvarteret Knaust med historiska platser och fï¿½remï¿½l.", creator, places);
         
         return route;
 	}
