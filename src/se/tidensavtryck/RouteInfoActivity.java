1fdbc661d912bfc6b93955ddac2e37c108ef3a67
Johan Nilsson <github@markupartist.com>
2011-03-18 19:32:32 +0100

    Basic flow

--- src/se/tidensavtryck/RouteInfoActivity.java
+++ src/se/tidensavtryck/RouteInfoActivity.java
@@ -7,19 +7,19 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class StartActivity extends Activity {
+public class RouteInfoActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.route_info);
         
-        Button button = (Button) findViewById(R.id.button_route_info);
+        Button button = (Button) findViewById(R.id.button_route);
         button.setOnClickListener(new OnClickListener() {
             
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(StartActivity.this, RouteInfoActivity.class);
+                Intent i = new Intent(RouteInfoActivity.this, RouteActivity.class);
                 startActivity(i);
             }
         });
