9cd6fb3161d2662c84de9b1c586bae9f76b7ff58
Johan Mjones <johan.mjones@gmail.com>
2011-03-18 23:20:16 +0100

    Made Route and User parcelable

--- src/se/tidensavtryck/model/Route.java
+++ src/se/tidensavtryck/model/Route.java
@@ -6,8 +6,11 @@ import java.util.LinkedList;
 import java.util.List;
 
 import android.location.Location;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.os.Parcelable.Creator;
 
-public class Route {
+public class Route implements Parcelable{
 
 	private String title;
 
@@ -45,6 +48,12 @@ public class Route {
 		// TODO Auto-generated constructor stub
 	}
 
+	public Route(Parcel parcel) {
+		this.title = parcel.readString();
+		this.description = parcel.readString();
+		this.creator = parcel.readParcelable(null);
+	}
+
 	public List<Place> getPlaces() {
 		return Collections.unmodifiableList(places);
 	}
@@ -64,4 +73,29 @@ public class Route {
 	public int getDurationInMinutes() {
 		return 54;
 	}
+
+	@Override
+	public int describeContents() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeString(this.title);
+		dest.writeString(this.description);
+		dest.writeParcelable(this.creator, 0);
+	}
+
+    public static final Creator<Route> CREATOR = new Creator<Route>() {
+        public Route createFromParcel(Parcel parcel) {
+            return new Route(parcel);
+        }
+
+        public Route[] newArray(int size) {
+            return new Route[size];
+        }
+    };
+
+
 }
--- src/se/tidensavtryck/model/User.java
+++ src/se/tidensavtryck/model/User.java
@@ -1,9 +1,36 @@
 package se.tidensavtryck.model;
 
-public class User {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class User implements Parcelable{
     private String userName;
 
     public User(String username) {
         this.userName = username;
     }
+
+	public User(Parcel parcel) {
+		this.userName = parcel.readString();
+	}
+
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeString(this.userName);
+	}
+
+    public static final Creator<User> CREATOR = new Creator<User>() {
+        public User createFromParcel(Parcel parcel) {
+            return new User(parcel);
+        }
+
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
 }
