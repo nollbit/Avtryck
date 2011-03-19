cbc1885f6d120f1473cbbfea6a2d4c815e2102b9
Johan Mjones <johan.mjones@gmail.com>
2011-03-19 01:26:29 +0100

    Loading all records, adding all places

--- src/se/tidensavtryck/gateway/RouteGateway.java
+++ src/se/tidensavtryck/gateway/RouteGateway.java
@@ -2,8 +2,11 @@ package se.tidensavtryck.gateway;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import android.content.res.AssetManager;
 import android.location.Location;
@@ -53,35 +56,49 @@ public class RouteGateway {
         Location loc2 = new Location("se.tidensavtryck");
         loc2.setLatitude(62.394369903217); 
         loc2.setLongitude(17.2816450479837);
+        place2.setGeoLocation(loc2);
 
         Location loc3 = new Location("se.tidensavtryck");
         loc3.setLatitude(62.3897829867526); 
         loc3.setLongitude(17.2995418371631);
+        place3.setGeoLocation(loc3);
         
         Location loc4 = new Location("se.tidensavtryck");
         loc4.setLatitude(62.391178326117); 
         loc4.setLongitude(17.3004228024664);
-        
+        place4.setGeoLocation(loc4);
+   
+        /*
         Location loc5 = new Location("se.tidensavtryck");
         loc5.setLatitude(62.3900820918969);
         loc5.setLongitude(17.3091424714359); 
+        place2.setGeoLocation(loc5);
+		*/
+        
+        Map<Integer, Place> placeRecordMap = new HashMap<Integer, Place>();
+        placeRecordMap.put(1, place1);
+        placeRecordMap.put(2, place2);
+        placeRecordMap.put(3, place3);
+        placeRecordMap.put(4, place4);
 
-        InputStream is = null;
-        List<Record> recordList = null;
+        List<Record> placeRecords = new ArrayList<Record>();
         try {
-            is = this.mAssetManager.open("place1.xml");
-            XMLPull pull = new XMLPull(is);
-            recordList = pull.parse();
+        	for (Integer i : placeRecordMap.keySet()) {
+        		String filename = String.format("place%d.xml", i);
+                InputStream is = this.mAssetManager.open("place1.xml");
+                XMLPull pull = new XMLPull(is);
+                placeRecordMap.get(i).setRecords(pull.parse());
+        		
+        	}
         } catch (IOException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
 
-        place1.setRecords(recordList);
-        Place p2 = new Place("Bla bla bla plats", loc2, 50, recordList);
-
         List<Place> places = new LinkedList<Place>();
         places.add(place1);
-        places.add(p2);
+        places.add(place2);
+        places.add(place3);
+        places.add(place4);
 
         route.setPlaces(places);
         routeList.add(route);
