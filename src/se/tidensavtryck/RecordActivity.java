b94ff5c3c9789bcce5e772acf82ffbb9edbfc58b
Patrik Åkerfeldt <patrik.akerfeldt@gmail.com>
2011-03-19 02:57:54 +0100

    Adding record activity to manifest

--- src/se/tidensavtryck/RecordActivity.java
+++ src/se/tidensavtryck/RecordActivity.java
@@ -1,7 +1,5 @@
 package se.tidensavtryck;
 
-import com.markupartist.android.widget.ActionBar;
-
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -14,14 +12,14 @@ public class RecordActivity extends Activity {
 	
         setContentView(R.layout.record);
 
-        initActionBar();
+//        initActionBar();
 
 	}
 	
     private void initActionBar() {
-        ActionBar actionBar = (ActionBar) findViewById(R.id.actionbar);
-        actionBar.setHomeAction(new ActionBar.IntentAction(
-                this, StartActivity.createIntent(this),
-                R.drawable.ic_actionbar_home_default));
+//        ActionBar actionBar = (ActionBar) findViewById(R.id.actionbar);
+//        actionBar.setHomeAction(new ActionBar.IntentAction(
+//                this, StartActivity.createIntent(this),
+//                R.drawable.ic_actionbar_home_default));
     }
 }
