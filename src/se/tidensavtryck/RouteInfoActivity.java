e21d3993ec08f2b64d33e14b0e2b6891a682ca63
Patrik Åkerfeldt <patrik.akerfeldt@gmail.com>
2011-03-19 13:29:06 +0100

    Ugliest demo hack ever written by a code monkey?

--- src/se/tidensavtryck/RouteInfoActivity.java
+++ src/se/tidensavtryck/RouteInfoActivity.java
@@ -46,7 +46,10 @@ public class RouteInfoActivity extends ListActivity {
         duration.setText(""+route.getDurationInMinutes());
         
         ImageView thumbnail = (ImageView) headerView.findViewById(R.id.routeThumbnail);
-        thumbnail.setBackgroundResource(R.drawable.route_thumbnail_example);
+        if(route.getTitle().equalsIgnoreCase("I ringmurens skugga")) 
+        	thumbnail.setBackgroundResource(R.drawable.route_thumbnail_example_visby);
+        else
+        	thumbnail.setBackgroundResource(R.drawable.route_thumbnail_example);
         
         Button button = (Button) headerView.findViewById(R.id.button_route);
         button.setOnClickListener(new OnClickListener() {
